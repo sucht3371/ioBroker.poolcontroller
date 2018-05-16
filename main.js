4b55c27fbff74b438de97f1abcfc2b5c60928990
Michael Voelker <mv@netcompsys.de>
2018-05-17 00:01:39 +0200

    Einstellungen

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ var port = adapter.config.port
 var interval = adapter.config.interval
 
 
-//var url='http://192.168.178.35:80/GetState.csv'
+
 
 
 //*********************************************************************
@@ -220,28 +220,6 @@ var stateanlegen = true;
    
 //#################################################################
 
-    //require("request")(url, function (error, response, result) 
-  //  {
-		   //   console.log(result);
-//		    result = result.replace(/ /g, '_');  //alle Leerzeichen durch Unterstrich ersetzten
-//		    var data=CSVToArray(result);         //CSV in ein Array einlesen
-//		    json = JSON.stringify(data);         //Array in einen String formatieren
-//		    var jdata = JSON.parse(json);        //Json Array erzeugen
-//		    var arr1 = jdata[0];                 // Array 0 von ingesamt 6, weil 6 Zeilen im CVS mit \n getrennt
-//		    var arr2 = jdata[1];                 // Array 1-5 haben je 42 einzelne  Daten
-//		    var arr3 = jdata[2];
-//		    var arr4 = jdata[3];
-//		    var arr5 = jdata[4];
-//		    var arr6 = jdata[5];
-   //}
-
-
-//#################################################################
-
-    
-    
-    //var url="http://" + host + ":" + port + "/GetState.csv"
-
 request(
 	{
 		url: "http://" + host + ":" + port + "/GetState.csv"
@@ -264,6 +242,58 @@ request(
 			adapter.log.info(arr2);
 			adapter.log.info('hallo');
 			adapter.log.info(content);
+			//#########################################
+			
+			  if (stateanlegen === true){
+    // User Variablen anlegen Achtung keine Punkte im Namen verwenden. 
+    var i=0;
+    for (i=0; i<=41;i++){
+        adapter.setObjectNotExists(+jdata[1][i],'', {
+        name: ''+jdata[1][i],
+        type: 'number',
+        unit: ''+jdata[2][i],
+        write: false,
+        read:  true
+        });
+    }
+    
+    adapter.log.info("Variablen angelegt");
+    
+    stateanlegen = false;
+  }
+  else {
+  
+  //SYSINFO Variablen mit aktuellen Werten beschreiben 
+    adapter.setState('sysinfo.VERSION', jdata[0][1]);
+    adapter.setState('sysinfo.CPU_TIME', parseFloat(Number(jdata[0][2]).toFixed(2)));
+    adapter.setState('sysinfo.RESET_ROOT_CAUSE', parseFloat(Number(jdata[0][3]).toFixed(2))); 
+    adapter.setState('sysinfo.NTP_FAULT_STATE', parseFloat(Number(jdata[0][4]).toFixed(2)));
+    adapter.setState('sysinfo.CONFIG_OTHER_ENABLE', parseFloat(Number(jdata[0][5]).toFixed(2)));
+    adapter.setState('sysinfo.DOSAGE_CNTRL', parseFloat(Number(jdata[0][6]).toFixed(2)));
+    adapter.setState('sysinfo.pH+_DOSAGE_RELAIS_ID', parseFloat(Number(jdata[0][7]).toFixed(2)));
+    adapter.setState('sysinfo.pH-_DOSAGE_RELAIS_ID', parseFloat(Number(jdata[0][8]).toFixed(2)));
+    adapter.setState('sysinfo.Chlor_DOSAGE_RELAIS_ID', parseFloat(Number(jdata[0][9]).toFixed(2)));
+  
+  
+   // User Variablen mit aktuellen Werten beschreiben
+   i=0; 
+    for (i=0; i<=41;i++){
+        //var wert = offset + ( gain * value);
+       var offset =  parseFloat(jdata[3][i]);
+       var gain =   parseFloat(jdata[4][i]);
+       var value =   parseFloat(jdata[5][i]);
+         var wert =  offset + ( gain * value);
+        adapter.setState(+jdata[1][i], parseFloat(Number(wert).toFixed(2)));
+         }    
+    
+    adapter.log.info("Variablen updated");    
+  }			
+			//#########################################
+			
+			
+			
+			
+			
 		} else 	{
 			adapter.log.error(error);
 		}		
