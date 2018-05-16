c45a6a86c39e53cea8021a0b1083c7be2c345c5c
Michael Voelker <mv@netcompsys.de>
2018-05-16 23:19:33 +0200

    Einstellungen

--- main.js
+++ main.js
@@ -252,8 +252,10 @@ request(
 		if (!error){
 			adapter.log.info(content);
 			content =content.replace(/ /g, '_'); 	 //alle Leerzeichen durch Unterstrich ersetzten
-	
-		
+			var data=CSVToArray(result);         //CSV in ein Array einlesen
+			json = JSON.stringify(data);         //Array in einen String formatieren
+			var arr1 = jdata[0];				// Array 0 von ingesamt 6, weil 6 Zeilen im CVS mit \n getrennt	
+			adapter.log.info(arr1);
 			adapter.log.info(content);
 		} else 	{
 			adapter.log.error(error);
