aa73e5b75e251e752579cf5ef60f7f4ce0b42ef8
Michael Voelker <mv@netcompsys.de>
2018-05-17 07:49:07 +0200

    Einstellungen

--- main.js
+++ main.js
@@ -248,13 +248,13 @@ request(
     // User Variablen anlegen Achtung keine Punkte im Namen verwenden. 
     //var i=0;
     //for (i=0; i<=41;i++){
-    //    adapter.setObjectNotExists(+jdata[1][i],'', {
-    //    name: ''+jdata[1][i],
-    //    type: 'number',
-    //    unit: ''+jdata[2][i],
-    //    write: false,
-    //    read:  true
-    //    });
+        adapter.setObjectNotExists(+jdata[1][i],'', {
+        name: ''+jdata[1][1],
+        type: 'number',
+        unit: ''+jdata[2][1],
+        write: false,
+        read:  true
+        });
     //}
     
     //adapter.log.info("Variablen angelegt");
