c79df69d9be9ea6e5811dc26da5b996ba17ed3f9
Michael Voelker <mv@netcompsys.de>
2018-05-17 22:28:55 +0200

    Einstellungen

--- main.js
+++ main.js
@@ -188,8 +188,7 @@ request(
 			
 			if (!error){
 
-  // 		while (true)
-	//	{					
+  	
 				content =content.replace(/ /g, '_'); 	 //alle Leerzeichen durch Unterstrich ersetzten
 				var data=CSVToArray(content);         //CSV in ein Array einlesen
 				json = JSON.stringify(data);         //Array in einen String formatieren
@@ -241,7 +240,7 @@ request(
 			   		adapter.log.info("warten");
 			   },10000);				
 			
-	//}	
+	
 			
 			
 		} 
